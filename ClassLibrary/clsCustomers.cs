470fd0ddd82a7469fb71e4a13243df753d857ec2
karishnaa <p2714644@my365.dmu.ac.uk>
2024-05-07 12:55:52 +0100

    design front end

--- ClassLibrary/clsCustomers.cs
+++ ClassLibrary/clsCustomers.cs
@@ -1,7 +1,17 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomers
     {
         public bool Active { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int CustomerID { get; set; }
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime DOB { get; set; }
+        public bool Subscribed { get; set; }
+        
     }
 }
