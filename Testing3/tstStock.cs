dd3e13a90d85cd5e37e6e1c14a1ad7d03de6c73a
dillydaboss1 <dilan5041@gmail.com>
2024-05-10 12:14:40 +0100

    2 tests

--- Testing3/tstStock.cs
+++ Testing3/tstStock.cs
@@ -1,16 +1,33 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.CodeDom;
 
 namespace Testing3
 {
     [TestClass]
     public class tstStock
     {
+        public bool Available { get; private set; }
+
         [TestMethod]
         public void InstanceOK()
         {
-            clsStock StockName = new clsStock();
+            //create an instance of the class we want to create
+            tstStock StockName = new tstStock();
+            //test to see that it exists
             Assert.IsNotNull(StockName);
+        }
+        [TestMethod]
+        public void AvailableOK()
+        {
+            //create an instance of the class we want to create
+            tstStock Available = new tstStock();
+            //create some test data to assign to the property
+            Boolean TestData = true;
+            //assign the data to the property
+            Available.Available = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(Available.Available, TestData);
         }
     }
 }
