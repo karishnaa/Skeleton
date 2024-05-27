0f73e38b9d9cff95dedc6b2fdc643822a9dd30d6
dillydaboss1 <dilan5041@gmail.com>
2024-05-27 23:51:35 +0100

    hi

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿USE [p2714644]
+GO
+
+DECLARE	@return_value Int
+
+EXEC	@return_value = [dbo].[sproc_tblStock_Count_GroupByPrice]
+
+SELECT	@return_value as 'Return Value'
+
+GO
---
+++ SQLQuery2.sql
@@ -0,0 +1,10 @@
+﻿USE [p2714644]
+GO
+
+DECLARE	@return_value Int
+
+EXEC	@return_value = [dbo].[sproc_tblStock_Count_GroupByQuantity]
+
+SELECT	@return_value as 'Return Value'
+
+GO
